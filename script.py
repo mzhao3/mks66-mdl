7671642879bc485b662f2e218444f4d4543021d5
mzhao3 <mzhao3@stuy.edu>
2019-05-14 15:36:57 -0400

    .

--- script.py
+++ script.py
@@ -48,4 +48,43 @@ def run(filename):
 
     print symbols
     for command in commands:
+        op = command['op']
+        args = command['args']
+
+        if op == 'move':
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        if op == 'sphere':
+            if command['cs'] == None:
+                cs = stack
+            else:
+                cs = command['cs']
+            print(cs)
+
+            if command['constants'] == None:
+                const = ".white"
+            else:
+                const = command['constants']
+
+
+            add_sphere(tmp,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+            matrix_mult( cs[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            #split constants into a, s, d
+            #constants name kar kdr ksr kag kdg ksg kab kdb ksb [r] [g] [b]
+
+            polygons = []
+
+        if op == 'push':
+            stack.append( [x[:] for x in stack[-1]] )
+        if op == 'pop':
+            stack.pop()
+        if op == 'display':
+            display(screen)
+        if op == 'save':
+            save_extension(screen, args[0]+".png")
         print command
